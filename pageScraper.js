5d03825bf77525d8493f5105706d84c490b288d3
KazuyaSerizawa <kserizawa081@gmail.com>
2022-01-15 15:45:31 +0900

    refactor: 💡 change logic to get updated items

--- pageScraper.js
+++ pageScraper.js
@@ -20,49 +20,35 @@ const scraperObject = {
     const min = 10000;
     const max = 60000;
 
-    const newItems = info.filter(
+    const latestItems = info.filter(
       (item) => min < item.price && item.price < max
     );
 
-    const lastItems = JSON.parse(fs.readFileSync("./last.json", "utf-8"));
-
-    const d = diff(newItems, lastItems);
-    // .map(
-    //   (obj) => Object.key(obj).length !== 0
-    // );
-
-    const addedItems = [];
-
-    for (const [key, value] of Object.entries(d)) {
-      console.log({
-        key,
-        value,
-      });
-      if (Object.keys[key]?.length === 0) {
-        const o = {};
-        o[key] = value;
-        addedItems.push(o);
-      }
-    }
-
-    const json = JSON.stringify(newItems);
-    fs.writeFile("last.json", json, "utf-8", () => {
-      console.log("addedItems", addedItems);
-      console.log(d);
+    const json = JSON.stringify(latestItems);
+    fs.writeFile("last.json", json, "utf-8", (err) => {
+      console.error(err);
     });
+
+    const oldItems = JSON.parse(fs.readFileSync("./last.json", "utf-8"));
+
+    const newItems = findUpdatedOrNewItems(latestItems, oldItems);
+
+    console.log("newItems", newItems);
   },
 };
 
-function diff(obj1, obj2) {
-  var result = {};
-  for (key in obj1) {
-    if (obj2[key] != obj1[key]) result[key] = obj2[key];
-    if (typeof obj2[key] == "array" && typeof obj1[key] == "array")
-      result[key] = arguments.callee(obj1[key], obj2[key]);
-    if (typeof obj2[key] == "object" && typeof obj1[key] == "object")
-      result[key] = arguments.callee(obj1[key], obj2[key]);
-  }
-  return result;
+function findUpdatedOrNewItems(latestItems, oldItems) {
+  const newItems = [];
+  latestItems.forEach((item) => {
+    if (
+      oldItems.filter(
+        (oldItem) => oldItem.price === item.price && oldItem.name === item.name
+      ).length === 0
+    ) {
+      newItems.push(item);
+    }
+  });
+  return newItems;
 }
 
 module.exports = scraperObject;
