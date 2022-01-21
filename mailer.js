0022d2796d19cd47c288ea54a5da57d82619cfa2
KazuyaSerizawa <kserizawa081@gmail.com>
2022-01-21 23:43:10 +0900

    feat: 🎸 try catch

--- mailer.js
+++ mailer.js
@@ -1,21 +1,25 @@
 const nodemailer = require("nodemailer");
 
 async function sendNotification(to, subject, text) {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASSWORD,
-    },
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+    });
 
-  const info = await transporter.sendMail({
-    from: "mercari-notification",
-    to,
-    subject,
-    text,
-  });
-  console.log(info);
+    const info = await transporter.sendMail({
+      from: "mercari-notification",
+      to,
+      subject,
+      text,
+    });
+    console.log(info);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 module.exports = { sendNotification };
